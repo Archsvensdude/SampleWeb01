8f4fcd2f40973174f546bec4a91ab88d5687889c
Archsvensdude (Ahn Joonyoung) <junepsgv1357@naver.com>
2024-08-27 20:16:13 +0900

    SampleWeb
    The Ingredient of SampleWeb

---
+++ SampleWeb/src/main/java/com/web/User.java
@@ -0,0 +1,23 @@
+package com.web;
+
+public class User {
+    private String uid;
+    private String pwd;
+    // Getters and Setters
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
